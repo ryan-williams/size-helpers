64f3ecf37742af0aa2be67b97feb5a02c700a57e
Ryan Williams <ryan@runsascoded.com>
2021-04-25 13:12:31 -0400

    attempt to recover from errors, use GNU sed

--- disk_tree.sh
+++ disk_tree.sh
@@ -5,10 +5,17 @@ set -e
 if [ $# -eq 0 ]; then
   "$0" "$PWD"
 else
+  # Need to use GNU sed; try to use it, if available
+  if which gsed &>/dev/null; then
+    sed="gsed"
+  else
+    echo "Not sure GNU sed is being used; empty results may ensue" >&2
+    sed="sed"
+  fi
   for arg in "$@"; do
     echo "Inspecting $arg" >&2
     pushd "$arg" &>/dev/null
-    find . -maxdepth 1 -print0 | \
+    (find . -maxdepth 1 -print0 || true) | \
     if which parallel &>/dev/null; then
       parallel -0 -k 'du -sh'
     else
@@ -16,10 +23,10 @@ else
       while IFS= read -r -d $'\0' path; do
         let ln=$ln+1
         if [ $ln -eq 1 ]; then continue; fi
-        du -sh "$path"
+        du -sh "$path" || true
       done
     fi | \
-    sed -n 's_\s\+\./\?\(.*\)$_\t\1_p' | \
+    "$sed" -n 's_\s\+\./\?\(.*\)$_\t\1_p' | \
     sort -h -k1 | \
     tee .disk-tree
     popd &>/dev/null
